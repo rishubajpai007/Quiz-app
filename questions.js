42825e2f16893f10faf22dabbaeada99178f135f
RISHU BAJPAI <83057142+rishubajpai007@users.noreply.github.com>
2023-10-01 00:05:20 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,59 @@
+let questions = [
+  {
+    numb: 1,
+    question: "What is the largest planet in our solar system?",
+    answer: "Jupiter",
+    options: [
+      "Jupiter",
+      "Saturn",
+      "Earth",
+      "Mars"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Which planet is known as the 'Red Planet'?",
+    answer: "Mars",
+    options: [
+      "Venus",
+      "Neptune",
+      "Uranus",
+      "Mars"
+    ]
+  },
+  {
+    numb: 3,
+    question: "What is the name of the largest moon of Saturn?",
+    answer: "Titan",
+    options: [
+      "Io",
+      "Titan",
+      "Ganymede",
+      "Callisto"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Which spacecraft was the first to land humans on the moon?",
+    answer: "Sputnik 1",
+    options: [
+      "Voyager 2",
+      "Apollo 11",
+      "Sputnik 1",
+      "Hubble Space Telescope"
+    ]
+  },
+  {
+    numb: 5,
+    question: "What is the name of the farthest dwarf planet in our solar system?",
+    answer: "Eris",
+    options: [
+      "Eris",
+      "Pluto",
+      "Haumea",
+      "Makemake"
+    ]
+  }
+];
+
+// You can use this code to display and manage these questions in your quiz app.
